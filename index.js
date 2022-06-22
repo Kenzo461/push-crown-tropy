05da0c27248848fc7e2cde92e743c68cf5af9061
Kenzo461 <107798869+Kenzo461@users.noreply.github.com>
2022-06-22 12:10:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ By : ${chalk.bold('Zexxy')} - Credit : @Zexxy & @Zexxy
       const trophy = data.User.SkillRating;
       const crown = data.User.Crowns;
 
-console.log(chalk.bgBlack(`\r[ ${moment().format('HH:mm:ss')} ] ${chalk.white(`User : ${username}`)} | ${chalk.yellow(`Trophy : ${trophy}`)} | ${chalk.magenta(`Crown : ${crown}`)}`));
+console.log(chalk.bgBlack(`\r[ ${moment().format('HH:mm:ss')} ] ${chalk.white(`User : ${username}`)} | ${chalk.white(`Trophy : ${trophy}`)} | ${chalk.white(`Crown : ${crown}`)}`));
       await sleep(6500);
 
     } else if (result == 'BANNED') {
