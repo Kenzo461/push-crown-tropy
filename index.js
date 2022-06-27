903bf2287d1011e668ef26ce084d2742f9017f6b
Kenzo461 <107798869+Kenzo461@users.noreply.github.com>
2022-06-27 16:11:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const fetch = require('node-fetch');
 const moment = require('moment');
 const chalk = require('chalk');
-const consola = require('consola');
+const CFonts = require('cfonts');
 const rs = require('readline-sync');
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const GoStumble = (auth) => new Promise((resolve, reject) => {
+(async () => {
+	console.clear()
+ CFonts.say(`Zexxy`, {
+            font: '3D',
+            align: 'left',
+            gradient: ['red', 'magenta']
+        })
+        await sleep(3000);
+        console.clear()
+        console.log(`${chalk.yellow(`Since Script 2022 - 5 - 21`)}`)
+console.log(`${chalk.white(`
+1 Push Trophy
+3 Push Crown
+Choose to use the available number`)} 
+`);
+const round = rs.question(`[+] Select Number  : `);
+    console.log('');
+    
+    const GoStumble = (auth) => new Promise((resolve, reject) => {
 
-  fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {
+  fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/'+round, {
     method: 'GET',
     headers: {
       'authorization': auth
@@ -26,23 +44,15 @@ const GoStumble = (auth) => new Promise((resolve, reject) => {
 
 });
 
-(async () => {
-
-  console.log(`
- Trophy And Crown Hack Safe! [ReEdit]
-By : ${chalk.bold('xDast#8745')} - Credit : @dkmpostor & @Eskey & Zenix
-`);
-
-  const auth = rs.question('Enter Authentication Code! : ');
-  chalk.bgYellow.bold('STARTING');
-
+  const auth = rs.question('Auth Token : ');
+  console.log('');
+console.clear()
   while (true) {
 
     const result = await GoStumble(auth);
     if (!result) {
 
-      chalk.bgRed.bold('Terjadi Kesalahan Saat Login Menggunakan Cookie,Mungkin Cookie Expired Atau Salah');
-      break
+      console.log(chalk.red(`\r[ ${moment().format('HH:mm:ss')} ] Maybe Auth Token Expired ?`));
 
     } else if (result.includes('User')) {
 
@@ -52,10 +62,29 @@ By : ${chalk.bold('xDast#8745')} - Credit : @dkmpostor & @Eskey & Zenix
       const trophy = data.User.SkillRating;
       const crown = data.User.Crowns;
 
-var _0x26f5b4=_0xa2f4;function _0xa2f4(_0x1dbece,_0x504334){var _0x191eec=_0x191e();return _0xa2f4=function(_0xa2f478,_0x55127d){_0xa2f478=_0xa2f478-0xf2;var _0x1b4966=_0x191eec[_0xa2f478];return _0x1b4966;},_0xa2f4(_0x1dbece,_0x504334);}function _0x191e(){var _0x127b0f=['⟩\x20Server\x20:\x20','1636692OPdmXH','798476nzRanp','336DXLqNl','50049gVHiXN','format','229244FsHwRC','⟩\x20User\x20:\x20','⟩\x20Trophy\x20:\x20','bold','bgYellow','Done\x20+1\x20Crown\x20+\x2030\x20Trophy\x20By\x20Zenix77','192749KhgBCo','3EFESnm','log','bgGreen','\x20\x0a\x0a','2460Eralwd','58944gXJLWk','bgBlue','1163355ylKjqO','bgWhite'];_0x191e=function(){return _0x127b0f;};return _0x191e();}(function(_0x41fe82,_0x15cfa5){var _0x5cf321=_0xa2f4,_0x22b85e=_0x41fe82();while(!![]){try{var _0x24cea8=-parseInt(_0x5cf321(0x101))/0x1+parseInt(_0x5cf321(0xfb))/0x2+-parseInt(_0x5cf321(0x102))/0x3*(parseInt(_0x5cf321(0xf7))/0x4)+-parseInt(_0x5cf321(0xf3))/0x5+-parseInt(_0x5cf321(0xf6))/0x6+parseInt(_0x5cf321(0xf8))/0x7*(-parseInt(_0x5cf321(0x107))/0x8)+-parseInt(_0x5cf321(0xf9))/0x9*(-parseInt(_0x5cf321(0x106))/0xa);if(_0x24cea8===_0x15cfa5)break;else _0x22b85e['push'](_0x22b85e['shift']());}catch(_0x1372d9){_0x22b85e['push'](_0x22b85e['shift']());}}}(_0x191e,0x386e7),console[_0x26f5b4(0x103)]('\x0a\x0a╭━━━━━━━━━━━━━╮\x0a\x0a┃╱╱╱╱╱╱╱╱┏┓╱╱╱┃\x0a\x0a┃╱╱╱┏┓╱╱┏╯┃╱╱╱┃\x0a\x0a┃╱╱┏┛┗┓╱┗┓┃╱╱╱┃\x0a\x0a┃╱╱┗┓┏┛╱╱┃┃╱╱╱┃\x0a\x0a┃╱╱╱┗┛╱╱╱┃┃╱╱╱┃\x0a\x0a┃╱╱╱╱╱╱╱╱┗┛╱╱╱┃\x0a\x0a╰━━━━━━━━━━━━━\x20\x0a\x0a'+console[_0x26f5b4(0x103)]('[\x20'+moment()[_0x26f5b4(0xfa)]('HH:mm:ss')+'\x20]')+'\x0a\x0a'+chalk['bgRed']['bold'](_0x26f5b4(0xfc)+username)+'\x20\x0a\x0a'+chalk[_0x26f5b4(0x104)][_0x26f5b4(0xfe)](_0x26f5b4(0xf5)+country)+_0x26f5b4(0x105)+chalk[_0x26f5b4(0xf2)][_0x26f5b4(0xfe)](_0x26f5b4(0xfd)+trophy)+_0x26f5b4(0x105)+chalk[_0x26f5b4(0xff)][_0x26f5b4(0xfe)]['red']('⟩\x20Crown\x20:\x20'+crown)+'\x0a\x0a'+chalk[_0x26f5b4(0xf4)]['black'](_0x26f5b4(0x100))),await sleep(0x1d4c));
-
+console.log(chalk.white(`\rTime : [ ${moment().format('HH:mm:ss')} ] ${chalk.red(`!`)}${chalk.white(`User : ${username}`)} | ${chalk.red(`!`)}${chalk.white(`Trophy : ${trophy}`)} | ${chalk.red(`!`)}${chalk.white(`Crown : ${crown}`)}
+${chalk.red(` Status : Succes✓`)}\n`));
+await sleep(0);
+console.log(chalk.white(`\rTime : [ ${moment().format('HH:mm:ss')} ] ${chalk.red(`!`)}${chalk.white(`User : ${username}`)} | ${chalk.red(`!`)}${chalk.white(`Trophy : ${trophy}`)} | ${chalk.red(`!`)}${chalk.white(`Crown : ${crown}`)}
+${chalk.red(` Status : Succes✓`)}\n`));
+await sleep(0);
+console.log(chalk.white(`\rTime : [ ${moment().format('HH:mm:ss')} ] ${chalk.red(`!`)}${chalk.white(`User : ${username}`)} | ${chalk.red(`!`)}${chalk.white(`Trophy : ${trophy}`)} | ${chalk.red(`!`)}${chalk.white(`Crown : ${crown}`)}
+${chalk.red(` Status : Succes✓`)}\n`));
+await sleep(0);
+console.log(chalk.green(`\rTime : [ ${moment().format('HH:mm:ss')} ] ${chalk.green(`!`)}${chalk.blue(`User : ${username}`)} | ${chalk.red(`!`)}${chalk.green(`Trophy : ${trophy}`)} | ${chalk.red(`!`)}${chalk.cyan(`Crown : ${crown}`)}
+${chalk.red(` Status : Succes✓`)}\n`));
+await sleep(0);
+console.log(chalk.white(`\rTime : [ ${moment().format('HH:mm:ss')} ] ${chalk.red(`!`)}${chalk.green(`User : ${username}`)} | ${chalk.red(`!`)}${chalk.red(`Trophy : ${trophy}`)} | ${chalk.red(`!`)}${chalk.green(`Crown : ${crown}`)}
+${chalk.red(` Status : Succes✓`)}\n`));
+await sleep(0);
+console.log(chalk.green(`\rTime : [ ${moment().format('HH:mm:ss')} ] ${chalk.blue(`!`)}${chalk.blue(`User : ${username}`)} | ${chalk.red(`!`)}${chalk.green(`Trophy : ${trophy}`)} | ${chalk.blue(`!`)}${chalk.white(`Crown : ${crown}`)}
+${chalk.red(` Status : Succes✓`)}\n`));
+await sleep(0);
+console.log(chalk.green(`\rTime : [ ${moment().format('HH:mm:ss')} ] ${chalk.blue(`!`)}${chalk.cyan(`User : ${username}`)} | ${chalk.red(`!`)}${chalk.cyan(`Trophy : ${trophy}`)} | ${chalk.green(`!`)}${chalk.blue(`Crown : ${crown}`)}
+${chalk.red(` Status : Succes✓`)}\n`));
+await sleep(0);
     } else if (result == 'BANNED') {
-      console.log(consola.error(`Akun lu di band makanya jan cheat`));
+      console.log(chalk.bgRed(`has your account been banned?`));
      break;
     }
   }
